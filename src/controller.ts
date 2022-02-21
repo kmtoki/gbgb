1025265eabe0f0740ad821f9a5b575ddb3eb7bf3
kmtoki <higumaido@gmail.com>
2022-02-21 21:17:32 +0900

    rewrite controller again

--- src/controller.ts
+++ src/controller.ts
@@ -3,31 +3,37 @@ import MBC from "./mbc.ts";
 
 export default class Controller {
   m: MBC;
-  buffer: U8;
+
+  down: U8;
+  up: U8;
+  right: U8;
+  left: U8;
+  start: U8;
+  select: U8;
+  b: U8;
+  a: U8;
 
   constructor(m: MBC) {
     this.m = m;
-    this.buffer = 0;
+    this.down = 1;
+    this.up = 1;
+    this.left = 1;
+    this.right = 1;
+    this.start = 1;
+    this.select = 1;
+    this.b = 1;
+    this.a = 1;
   }
 
   execute() {
     let jm = this.m.ram[Reg.JOYP] >> 4 & 0b11;
-    let bm = this.buffer >> 4 & 0b11;
-
-    if (jm == bm) {
-      this.m.ram[Reg.JOYP] = this.buffer;
-      // selected action
-      //if (jm == 0b01) {
-      //  this.m.ram[Reg.JOYP] = this.buffer;
-      //}
-      //// selected direction
-      //else if (jm == 0b10) {
-      //  this.m.ram[Reg.JOYP] = this.buffer;
-      //}
-      //else {
-      //  this.m.ram[Reg.JOYP] = this.buffer;
-      //  //this.buffer = this.m.ram[Reg.JOYP];
-      //}
+    if (jm == 0b01) {
+      this.m.ram[Reg.JOYP] = 
+        0b00010000 | (this.start << 3) | (this.select << 2) | (this.b << 1) | this.a;
+    }
+    else if (jm == 0b10) {
+      this.m.ram[Reg.JOYP] = 
+        0b00100000 | (this.down << 3) | (this.up << 2) | (this.left << 1) | this.right;
     }
     else {
       this.m.ram[Reg.JOYP] |= 0b1111; 
@@ -38,211 +44,61 @@ export default class Controller {
     switch (c) {
       case "w":
         this.up = 1;
-        this.matrix_select_arrow = 1;
         break;
       case "s":
         this.down = 1;
-        this.matrix_select_arrow = 1;
         break;
       case "a":
         this.left = 1;
-        this.matrix_select_arrow = 1;
         break;
       case "d":
         this.right = 1;
-        this.matrix_select_arrow = 1;
         break;
       case "k":
         this.a = 1;
-        this.matrix_select_action = 1;
         break;
       case "j":
         this.b = 1;
-        this.matrix_select_action = 1;
         break;
       case "x":
         this.start = 1;
-        this.matrix_select_action = 1;
         break;
       case "z":
         this.select = 1;
-        this.matrix_select_action = 1;
         break;
       default:
         return;
     }
-
-    this.m.ram[Reg.IF] |= 0b10000;
   }
 
   press(c: string) {
     switch (c) {
      case "w":
         this.up = 0;
-        this.matrix_select_arrow = 0;
-        //this.matrix_select_action = 1;
         break;
       case "s":
         this.down = 0;
-        this.matrix_select_arrow = 0;
-        //this.matrix_select_action = 1;
         break;
       case "a":
         this.left = 0;
-        this.matrix_select_arrow = 0;
-        //this.matrix_select_action = 1;
         break;
       case "d":
         this.right = 0;
-        this.matrix_select_arrow = 0;
-        //this.matrix_select_action = 1;
         break;
       case "k":
         this.a = 0;
-        this.matrix_select_action = 0;
-        //this.matrix_select_arrow = 1;
         break;
       case "j":
         this.b = 0;
-        this.matrix_select_action = 0;
-        //this.matrix_select_arrow = 1;
         break;
       case "x":
         this.start = 0;
-        this.matrix_select_action = 0;
-        //this.matrix_select_arrow = 1;
         break;
       case "z":
         this.select = 0;
-        this.matrix_select_action = 0;
-        //this.matrix_select_arrow = 1;
         break;
       default:
         return;
-    }
-
-    this.m.ram[Reg.IF] |= 0b10000;
-  }
-
-  releaseAll() {
-    this.buffer |= 0b111111;
-    //this.m.ram[Reg.IF] |= 0b10000;
-  }
-
-  pressAll() {
-    this.buffer &= ~0b1111;
-    //this.m.ram[Reg.IF] |= 0b10000;
-  }
-
-  set matrix_select_action(u: U8) {
-    if (u == 0) {
-      this.buffer &= ~0b00100000;
-    } else {
-      this.buffer |=  0b00100000;
-    }
-  }
-
-  set matrix_select_arrow(u: U8) {
-    if (u == 0) {
-      this.buffer &= ~0b00010000;
-    } else {
-      this.buffer |=  0b00010000;
-    }
-  }
-
-  get start() {
-    return (this.buffer >> 5 & 1) |
-      (this.buffer >> 3 & 1);
-  }
-  set start(u: U8) {
-    if (u == 0) {
-      this.buffer &= ~0b00001000;
-    } else {
-      this.buffer |= 0b00001000;
-    }
-  }
-
-  get select() {
-    return (this.buffer >> 5 & 1) |
-      (this.buffer >> 2 & 1);
-  }
-  set select(u: U8) {
-    if (u == 0) {
-      this.buffer &= ~0b00000100;
-    } else {
-      this.buffer |= 0b00000100;
-    }
-  }
-
-  get b() {
-    return (this.buffer >> 5 & 1) |
-      (this.buffer >> 1 & 1);
-  }
-  set b(u: U8) {
-    if (u == 0) {
-      this.buffer &= ~0b00000010;
-    } else {
-      this.buffer |= 0b00000010;
-    }
-  }
-
-  get a() {
-    return (this.buffer >> 5 & 1) |
-      (this.buffer & 1);
-  }
-  set a(u: U8) {
-    if (u == 0) {
-      this.buffer &= ~0b00000001;
-    } else {
-      this.buffer |= 0b00000001;
-    }
-  }
-
-  get down() {
-    return (this.buffer >> 4 & 1) |
-      (this.buffer >> 3 & 1);
-  }
-  set down(u: U8) {
-    if (u == 0) {
-      this.buffer &= ~0b00001000;
-    } else {
-      this.buffer |= 0b00001000;
-    }
-  }
-
-  get up() {
-    return (this.buffer >> 4 & 1) |
-      (this.buffer >> 2 & 1);
-  }
-  set up(u: U8) {
-    if (u == 0) {
-      this.buffer &= ~0b00000100;
-    } else {
-      this.buffer |= 0b00000100;
-    }
-  }
-
-  get left() {
-    return (this.buffer >> 4 & 1) |
-      (this.buffer >> 1 & 1);
-  }
-  set left(u: U8) {
-    if (u == 0) {
-      this.buffer &= ~0b00000010;
-    } else {
-      this.buffer |= 0b00000010;
-    }
-  }
-
-  get right() {
-    return (this.buffer >> 4 & 1) |
-      (this.buffer & 1);
-  }
-  set right(u: U8) {
-    if (u == 0) {
-      this.buffer &= ~0b00000001;
-    } else {
-      this.buffer |= 0b00000001;
     }
   }
 
